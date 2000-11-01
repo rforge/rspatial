a6046cc46d1c3e3eff14c7614b34f72c76cb62a6
rsb <rsb@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2000-11-01 21:26:02 +0000

    changed licence
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rspatial@40 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- splancs/src/cncvwt.f
+++ splancs/src/cncvwt.f
@@ -1,3 +1,18 @@
+C Copyright Barry Rowlingson <b.rowlingson@lancaster.ac.uk> and 
+C Peter Diggle (c) 1991-3; http://www.maths.lancs.ac.uk/~rowlings/Splancs/
+C R port: copyright 1998-2000 by Roger S. Bivand
+C
+C  This program is free software; you can redistribute it and/or modify
+C  it under the terms of the GNU General Public License as published by
+C  the Free Software Foundation; either version 2 of the License, or
+C  (at your option) any later version.
+C
+C  This program is distributed in the hope that it will be useful,
+C  but WITHOUT ANY WARRANTY; without even the implied warranty of
+C  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+C  GNU General Public License for more details.
+
+
       function cncvwt(x,y,r,xp,yp,np)
 c
 c compute the weight given to a point at x,y according to how much
