db4e4cc44e277021d57cbda9ff17f40c1be3f124
rsbivand <rsbivand@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2004-11-01 15:15:55 +0000

    pre lancs
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rspatial@134 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- spgwr/src/gwe.c
+++ spgwr/src/gwe.c
@@ -6,6 +6,9 @@
 #include <Rmath.h>
 #include <R_ext/Applic.h>
 
+void gw_gcdist(double *lon1, double *lon2, double *lat1, double *lat2, 
+		double *dist);
+
 void gw_dists(double *u, double *v, double *uout, double *vout, 
 		int *n, double *dists);
 
@@ -37,4 +40,47 @@ void gw_dists(double *u, double *v, double *uout, double *vout,
 		dists[j] = pythag((u[j]-uout[0]), (v[j]-vout[0]));
 }
 
+
+/* http://home.att.net/~srschmitt/greatcircle.html */
+
+void gw_gcdist(double *lon1, double *lon2, double *lat1, double *lat2, 
+		double *dist) {
+	
+    double F, G, L, sinG2, cosG2, sinF2, cosF2, sinL2, cosL2, S, C;
+    double w, R, a, f, D, H1, H2;
+    double lat1R, lat2R, lon1R, lon2R, DE2RA;
+    
+    DE2RA = M_PI/180;
+    a = 6378.137;              /* WGS-84 equatorial radius in km */
+    f = 1.0/298.257223563;     /* WGS-84 ellipsoid flattening factor */
+    
+    lat1R = lat1[0]*DE2RA;
+    lat2R = lat2[0]*DE2RA;
+    lon1R = lon1[0]*DE2RA;
+    lon2R = lon2[0]*DE2RA;
+    
+    F = ( lat1R + lat2R )/2.0;
+    G = ( lat1R - lat2R )/2.0;
+    L = ( lon1R - lon2R )/2.0;
+
+    sinG2 = R_pow_di( sin( G ), 2 );
+    cosG2 = R_pow_di( cos( G ), 2 );
+    sinF2 = R_pow_di( sin( F ), 2 );
+    cosF2 = R_pow_di( cos( F ), 2 );
+    sinL2 = R_pow_di( sin( L ), 2 );
+    cosL2 = R_pow_di( cos( L ), 2 );
+
+    S = sinG2*cosL2 + cosF2*sinL2;
+    C = cosG2*cosL2 + sinF2*sinL2;
+
+    w = atan( sqrt( S/C ) );
+    R = sqrt( S*C )/w;
+
+    D = 2*w*a;
+    H1 = ( 3*R - 1 )/( 2*C );
+    H2 = ( 3*R + 2 )/( 2*S );
+
+    dist[0] = D*( 1 + f*H1*sinF2*cosG2 - f*H2*cosF2*sinG2 ); 
+
+}
 
