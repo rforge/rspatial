c970ab03e22ac80492dab41b327ea5ad9b8c54a9
rsbivand <rsbivand@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2004-11-05 19:17:08 +0000

    first commit of SRDF
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rspatial@144 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ sp/R/Class-SpatialRingsDataFrame.R
@@ -0,0 +1,15 @@
+
+setClass("SpatialRingsDataFrame",
+	representation("SpatialRings", data = "data.frame", 
+		labelPoints = "ANY"),
+	prototype = list(new("SpatialRings"), data = data.frame(),
+		labelPoints = NULL),
+	validity = function(object) {
+		if (!inherits(object@data, "data.frame"))
+			stop("data should be of class data.frame")
+		if (nrow(object@data) != length(object@polygons))
+		  stop("number of rows in data.frame and SpatialPoints don't match")
+		return(TRUE)
+	}
+)
+
