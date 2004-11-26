5e0309aa39e1d40f5e44a84fe28713105fea408b
rsbivand <rsbivand@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2004-11-26 09:35:11 +0000

    revised tests/*.Rout.save
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rspatial@162 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- sp/tests/fail1.R
+++ sp/tests/fail1.R
@@ -3,29 +3,39 @@ data(meuse)
 x = meuse
 
 coordinates(x) <- c("x", "y")
-try(proj4string(x) <- 1.5)
+res <- try(proj4string(x) <- 1.5)
+print(res)
 
-try(coordinates(a) <- cbind(1:10, 10:1))
+res <- try(coordinates(a) <- cbind(1:10, 10:1))
+print(res)
 # fails because a is not found; passes if a assigned NULL, see pass1.R
 
 x = meuse
 # invalid coordinate formulae:
-try(coordinates(x) <- ~log(x)+sqrt(y)) # no expressions allowed
-try(coordinates(x) <- ~x+y+z) # z is not present
+res <- try(coordinates(x) <- ~log(x)+sqrt(y)) # no expressions allowed
+print(res)
+res <- try(coordinates(x) <- ~x+y+z) # z is not present
+print(res)
 x$x2 = x$x^2
 x$y2 = x$y^2
-try(coordinates(x) <- ~x+y+x2+y2) # 4D not allowed
-try(coordinates(x) <- ~x) # 1D not allowed
+res <- try(coordinates(x) <- ~x+y+x2+y2) # 4D not allowed
+print(res)
+res <- try(coordinates(x) <- ~x) # 1D not allowed
+print(res)
 # is.na.sp.coords
 a = data.frame(cbind(xx=c(1,NA,2,10),yy=c(2,NA,NA,20)))
-try(coordinates(a) <- c("xx", "yy")) # should fail!
+res <- try(coordinates(a) <- c("xx", "yy")) # should fail!
+print(res)
 
 x = meuse[1:4,]
 coordinates(x) = c(1,2)
 # this should fail -- zinc is not a row:
 #try(q <- x["zinc",])
 # this will issue a warning under S-Plus, or a silent rename under R
-try(x[c("zinc", "x", "copper", "zinc")])
+res <- try(x[c("zinc", "x", "copper", "zinc")])
+print(res)
 
 xx = data.frame(x=1:10, y=1:10)
-try(coordinates(xx) <- c("x", "y")) # leaves data.frame empty: use SpatialPoints!
+res <- try(coordinates(xx) <- c("x", "y")) 
+  #leaves data.frame empty: use SpatialPoints
+print(res)
