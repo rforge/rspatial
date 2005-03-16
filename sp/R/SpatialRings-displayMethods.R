8e034f4ff8c958b5ed38f96d779cfbe5804fe357
rsbivand <rsbivand@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2005-03-16 18:31:41 +0000

    more SRings
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rspatial@238 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- sp/R/SpatialRings-displayMethods.R
+++ sp/R/SpatialRings-displayMethods.R
@@ -14,53 +14,52 @@ plot.SpatialRings <- function(x, col, border = par("fg"), add=FALSE, xlim=NULL,
 	if (!add) plot.Spatial(x, xlim=xlim, ylim=ylim, asp=asp, ...)
 
 	if (missing(col)) col <- NA
-	n <- length(x@polygons)
+	n <- length(getSRpolygonsSlot(x))
 	if (length(border) != n)
 		border <- rep(border, n, n)
-	pO <- x@plotOrder
+	polys <- getSRpolygonsSlot(x)
+	pO <- getSRplotOrderSlot(x)
 	if (!is.null(density)) {
 		if (length(density) != n)
 			density <- rep(density, n, n)
 		if (length(angle) != n)
 			angle <- rep(angle, n, n)
 		for (j in pO) 
-				.polygonRingHoles(x@polygons[[j]], border = border[j], 
-				xpd = xpd, density = density[j], angle = angle[j], pbg = pbg) 
+			.polygonRingHoles(polys[[j]], border = border[j], 
+			xpd = xpd, density = density[j], angle = angle[j], 
+			pbg = pbg) 
 	} else {
-		if (length(col) != n)
-			col <- rep(col, n, n)
+		if (length(col) != n) col <- rep(col, n, n)
 		for (j in pO) 
-			.polygonRingHoles(x@polygons[[j]], col=col[j], 
-				border=border[j], xpd = xpd, pbg = pbg)
+			.polygonRingHoles(polys[[j]], col=col[j], 
+			border=border[j], xpd = xpd, pbg = pbg)
 	}
 }
 
-.polygonRingHoles <- function(R4, col=NA, border=NULL, xpd=NULL, density=NULL,
+.polygonRingHoles <- function(Sr, col=NA, border=NULL, xpd=NULL, density=NULL,
 	angle=45, pbg) {
-	if (!is(R4, "Ring4")) 
-		stop("Not a Ring4 object")
-	coords <- R4@coords
-	nParts <- nParts.matrix(coords)
-	xyList <- .NAmat2xyList(coords)
+	if (!is(Sr, "Srings")) 
+		stop("Not an Srings object")
 	if (is.na(col)) hatch <- TRUE
 	else hatch <- FALSE
-	pO <- R4@plotOrder
+	pO <- getSringsplotOrderSlot(Sr)
+	polys <- getSringsSringsSlot(Sr)
 	
 	for (i in pO) {
 		if (hatch) {
-			if (R4@ringDir[i] == 1)
-				.polygon(xyList[[i]], 
+			if (!getSringHoleSlot(polys[[i]]))
+				.polygon(getSringCoordsSlot(polys[[i]]), 
 					border = border, xpd = xpd, 
 					density = density, angle = angle,
 					hatch=TRUE)
-			else .polygon(xyList[[i]], 
+			else .polygon(getSringCoordsSlot(polys[[i]]), 
 					border = border, xpd = xpd, col=pbg, 
 					density = NULL)
 		} else {
-			if (R4@ringDir[i] == 1)
-				.polygon(xyList[[i]], 
+			if (!getSringHoleSlot(polys[[i]]))
+				.polygon(getSringCoordsSlot(polys[[i]]), 
 					border = border, xpd = xpd, col=col)
-			else .polygon(xyList[[i]], 
+			else .polygon(getSringCoordsSlot(polys[[i]]), 
 				border = border, xpd = xpd, col=pbg)
 		}
 	}
