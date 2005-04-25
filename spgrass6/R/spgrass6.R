3c635070c50d66ec654f688e92cb22f93e52d274
rsbivand <rsbivand@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2005-04-25 09:19:35 +0000

    rasters
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rspatial@284 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- spgrass6/R/spgrass6.R
+++ spgrass6/R/spgrass6.R
@@ -123,11 +123,59 @@ readFLOAT6 <- function(vname) {
 	res
 }
 
-writeCELL6 <- function(var, vname) {
-
+writeRast6 <- function(df, vname, zcol = 3, xcol = 1, ycol = 2, NODATA=-9999,
+    tol=1e-8) {
+	if (!is.numeric(df[,zcol])) stop("only numeric columns may be exported")
+	tmpfl <- tempfile()
+	arcGrid(df, tmpfl, zcol=zcol, xcol=xcol, ycol=ycol, NODATA=NODATA,
+		tol=tol)
+	system(paste("r.in.gdal -o input=", tmpfl, " output=", vname, sep=""))
 }
 
-writeFLOAT6 <- function(var, vname) {
-
+arcGrid <- function (xyz, file, zcol = 3, xcol = 1, ycol = 2, NODATA=-9999,
+    tol=1e-8) 
+# original code by Edzer Pebesma
+{
+    if (ncol(xyz) < 3) 
+        stop("xyz object should have at least three columns")
+    z = xyz[, zcol]
+    z[!is.finite(z)] <- NODATA
+    x = xyz[, xcol]
+    y = xyz[, ycol]
+    xx = sort(unique(x))
+    yy = sort(unique(y))
+    my = match(y, yy)
+    nx = length(xx)
+    ny = length(yy)
+    nmax = max(nx, ny)
+    difx = diff(xx)
+    if (diff(range(unique(difx))) > tol) 
+        stop("x intervals are not constant")
+    dify = diff(yy)
+    if (diff(range(unique(dify))) > tol) 
+        stop("y intervals are not constant")
+    dx = difx[1]
+    dy = dify[1]
+    ratio = (nx * dx)/(ny * dy)
+    xmin = min(xx)
+    xmax = max(xx)
+    xrange = xmax - xmin
+    ymin = min(yy)
+    ymax = max(yy)
+    yrange = ymax - ymin
+    zzz <- file(file, "w")
+    cat("NCOLS ", nx, "\n", file=zzz)
+    cat("NROWS ", ny, "\n", file=zzz)
+    cat("XLLCENTER ", xmin, "\n", file=zzz)
+    cat("YLLCENTER ", ymin, "\n", file=zzz)
+    cat("CELLSIZE" , dx, "\n", file=zzz)
+    cat("NODATA_VALUE" , NODATA, "\n", file=zzz)
+    for(i in ny:1) {
+	zz <- rep(NODATA, nx)
+	mmy <- which(my == i)
+	zz[match(x[mmy], xx)] <- z[mmy]
+        write(zz, file=zzz, ncolumns=nx, append=TRUE)
+    }
+    close(zzz)
 }
 
