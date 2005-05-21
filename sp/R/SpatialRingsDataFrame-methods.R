24383dd272ce6266078d8926e19bf56c0ac12d01
rsbivand <rsbivand@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2005-05-21 18:47:13 +0000

    ID checks for Srings etc
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rspatial@315 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- sp/R/SpatialRingsDataFrame-methods.R
+++ sp/R/SpatialRingsDataFrame-methods.R
@@ -1,5 +1,16 @@
-SpatialRingsDataFrame <- function(Sr, data)
-	new("SpatialRingsDataFrame", Sr, data = data)
+SpatialRingsDataFrame <- function(Sr, data, match.ID = TRUE) {
+	if (match.ID) {
+		Sr_IDs <- getSRSringsIDSlots(Sr)
+		data_IDs <- row.names(data)
+		mtch <- match(Sr_IDs, data_IDs)
+		if (any(is.na(mtch)))
+			stop("row.names of data and Srings IDs do not match")
+		if (length(unique(mtch)) != length(Sr_IDs))
+			stop("row.names of data and Srings IDs do not match")
+		data <- data[mtch, ]
+	}
+	new("SpatialRingsDataFrame", Sr, data=data)
+}
 
 "rings<-" = function(object, value) {
 	if (!is(value, "SpatialRings"))
