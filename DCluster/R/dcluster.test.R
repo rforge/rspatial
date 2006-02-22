a64499caf78c0cf09a3e5a5f8e8bd5f7b9f9eb7d
vir <vir@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2006-02-22 19:20:56 +0000

    Finished output of bootstrap tests. I still need to add a few more lines to summarise results from opgam().
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rspatial@457 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- DCluster/R/dcluster.test.R
+++ DCluster/R/dcluster.test.R
@@ -5,23 +5,61 @@
 
 dcluster.test<-function(b)
 {
-	print("CHECK if we have a boot object or a data.frame or WHAT!!")
 
+cl<-attr(b, "class")#Get class
+if(grep("boot",cl))
+{
 
 	#Parameters used in the call to boot(...)
 	p<-as.character(b$call)
 
 	#Test computed
-	test<-strsplit(p[3], ".", fixed=TRUE)
+	test<-(strsplit(p[3], ".", fixed=TRUE))[[1]][1]
 
-	if(grep("moran", p))
+	#Title
+	title<-switch(test,
+	"achisq"="Chi-square test for overdispertion",
+	"pottwhitt"="Potthoff-Whittinghill's test of overdispertion",
+	"moranI"="Moran's I test of spatial autocorrelation",
+	"gearyc"="Geary's c test of spatial autocorrelation",
+	"whittermore"="Whittermore's test of global clustering",
+	"tango"="Tango's test of global clustering"
+	)
+
+	#Sampling model
+	m<-(strsplit(p[6], ".", fixed=TRUE))[[1]][1]
+	if(grep("parametric", b$sim))
 	{
-	print("Results of Moran's I test for spatial autocorrelation")
-	print("-------->Type of boots.")
-	print("-------->Model used when sampling")
-	print(c("Number of simulations:", b$R))
-	print(c("Statistic: ", b$t0))
-	print(c("p-value : ", (1+sum(b$t0<b$t))/(b$R+1)))
+		model<-switch(m, 
+			"multinom"="Multinomial",
+			"poisson"="Poisson",
+			"negbin"="Negative Binomial"
+			)
 	}
+	else
+	{
+		model<-"permutation"
+	}
+	
+
+	pvalue<-(1+sum(as.numeric(b$t0)<as.numeric(b$t)))/(b$R+1)
+
+	cat(title, '\n\n')
+	cat("\tType of boots.:", b$sim,'\n')
+	cat("\tModel used when sampling:", model, '\n')
+	cat("\tNumber of simulations:", b$R, '\n')
+	cat("\tStatistic: ", b$t0, '\n')
+	cat("\tp-value : ", pvalue, '\n')
+}
+else
+{
+if(grep("data.frame",cl))
+{
+
+	print("Results from data frames returned by opgam")
+
+
+}
+}
 
 }
