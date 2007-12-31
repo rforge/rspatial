7bbff62d8601e54cf7a08136030b03339bc99ae6
vir <vir@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2007-12-31 00:02:34 +0000

    This file contains a few functions to estimate the 'best' bandwidth for a case-control point pattern. The procedures implemented are described in Kelsall and Diggle (1998).
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rspatial@664 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ SpatialEpiR/R/cvratio.R
@@ -0,0 +1,152 @@
+
+cvkernratio<-function(ccpts, ncases, ncontrols, pbdry, h, gt, edge=FALSE)
+{
+	nh<-length(h)
+	cv<-rep(NA, nh)
+	int<-rep(NA, nh)
+
+	idxcases<-1:ncases
+	idxcontrols<-ncases+1:ncontrols
+
+	gtfilter<-inpip(as.points(coordinates(gt)), pbdry)
+	cellsize<-prod(gt@cellsize)
+
+	setkernel("gaussian")
+	
+	for(bw in 1:nh)
+	{
+		hbw<-h[bw]
+
+		mcases<-matrix(NA, ncol=2, nrow=ncases) 
+		for(i in idxcases)
+		{
+			#print(i)
+mcases[i,1]<-lambdahat(ccpts[idxcases[-i],], hbw, matrix(ccpts[i,], ncol=2), pbdry, edge=edge)$lambda/(ncases-1)
+mcases[i,2]<-lambdahat(ccpts[idxcontrols,], hbw, matrix(ccpts[i,], ncol=2), pbdry, edge=edge)$lambda/ncontrols
+		}
+
+
+		mcontrols<-matrix(NA, ncol=2, nrow=ncontrols) 
+		for(i in idxcontrols)
+		{
+			#print(i)
+mcontrols[i-ncases,1]<-lambdahat(ccpts[idxcases,], hbw, matrix(ccpts[i,], ncol=2), pbdry, edge=edge)$lambda/ncases
+mcontrols[i-ncases,2]<-lambdahat(ccpts[idxcontrols[-(i-ncases)],], hbw, matrix(ccpts[i,], ncol=2), pbdry, edge=edge)$lambda/(ncontrols-1)
+		}
+
+
+		rcases<-log(mcases[,1]/mcases[,2])		
+		rcontrols<-log(mcontrols[,1]/mcontrols[,2])
+
+#		int[bw]<-computeint(ccpts[idxcases,], ccpts[idxcontrols,], pbdry, hbw, gt, gtfilter, cellsize)
+		int[bw]<-computeint2(ccpts[idxcases,], ccpts[idxcontrols,], pbdry, hbw)
+
+cv[bw]<- -int[bw] -(2/ncases)*sum(rcases/mcases[,1]) + (2/ncontrols)*sum(rcontrols/mcontrols[,2])
+
+	print(hbw)
+	}
+
+	return(list(int, cv))
+}
+
+
+computeint<-function(cases, controls, pbdry, bw, gt, gtfilter, cellsize, edge=FALSE){
+
+#	kcases<-spkernel2d(cases, pbdry, h0=bw, gt)/nrow(cases)
+#	kcontrols<-spkernel2d(controls, pbdry, h0=bw, gt)/nrow(controls)
+#	kratio<- log(kcases[gtfilter]/kcontrols[gtfilter])
+
+	kcases<-lambdahat(cases, bw, as.points(coordinates(gt)[gtfilter,]), pbdry, edge=edge)$lambda/nrow(cases)
+	kcontrols<-lambdahat(controls, bw, as.points(coordinates(gt)[gtfilter,]), pbdry, edge=edge)$lambda/nrow(controls)
+
+	kratio<- log(kcases/kcontrols)
+
+	int<-  sum(kratio*kratio, na.rm=TRUE)*cellsize
+
+	return(int)
+
+}
+
+#CV for the binary regression estimator
+cvbinreg<-function(ccpts, ncases, ncontrols, pbdry, h, edge=FALSE)
+{
+	nh<-length(h)
+	cv<-rep(NA, nh)
+	n<-ncases+ncontrols
+
+	y<-c(rep(1, ncases), rep(0, ncontrols))
+	idxcases<-1:ncases
+	idxcontrols<-ncases+1:ncontrols
+
+	setkernel("gaussian")
+	
+	for(bw in 1:nh)
+	{
+		hbw<-h[bw]
+
+		mcases<-matrix(NA, ncol=2, nrow=ncases) 
+		for(i in idxcases)
+		{
+mcases[i,1]<-lambdahat(ccpts[idxcases[-i],], hbw, matrix(ccpts[i,], ncol=2), pbdry, edge=edge)$lambda
+mcases[i,2]<-lambdahat(ccpts[idxcontrols,], hbw, matrix(ccpts[i,], ncol=2), pbdry, edge=edge)$lambda
+		}
+
+
+		mcontrols<-matrix(NA, ncol=2, nrow=ncontrols) 
+		for(i in idxcontrols)
+		{
+mcontrols[i-ncases,1]<-lambdahat(ccpts[idxcases,], hbw, matrix(ccpts[i,], ncol=2), pbdry, edge=edge)$lambda
+mcontrols[i-ncases,2]<-lambdahat(ccpts[idxcontrols[-(i-ncases)],], hbw, matrix(ccpts[i,], ncol=2), pbdry, edge=edge)$lambda
+		}
+
+
+		pcases<-mcases[,1]/(mcases[,1]+mcases[,2])
+		pcontrols<-mcontrols[,1]/(mcontrols[,1]+mcontrols[,2])
+
+
+		cv[bw]<- (-1/n)*sum(c(log(pcases), log(1-pcontrols)))
+
+	print(hbw)
+	}
+
+	return(exp(cv))
+}
+
+
+
+#Compute the integral using adapt
+
+
+logr<-function(pts, cases, controls, pbdry, bw, edge=FALSE)
+{
+
+	pts<-matrix(pts, ncol=2)
+
+	if(!inout(pts, pbdry))
+	{
+		kratio<-0
+	}
+	else
+	{
+        kcases<-lambdahat(cases, bw, pts, pbdry, edge=edge)$lambda/nrow(cases)
+        kcontrols<-lambdahat(controls, bw, pts, pbdry, edge=edge)$lambda/nrow(controls)
+
+        kratio<- log(kcases/kcontrols)
+	}
+
+	return(kratio*kratio)
+}
+
+
+
+
+computeint2<-function(cases, controls, pbdry, bw)
+{
+	int<-adapt(2, apply(pbdry,2,min),  apply(pbdry,2,max), functn=logr,
+		cases=cases, controls=controls, pbdry=pbdry, bw=bw)
+
+        return(int$value)
+}
+
+
+
