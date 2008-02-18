37575ee623da7cace0a5fea080d9391c5d8f31b5
rsbivand <rsbivand@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2008-02-18 19:43:41 +0000

    RMSE and gwr.Gauss fixes, note default kernel changed and default CV score changed
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rspatial@681 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- spgwr/R/zzz.R
+++ spgwr/R/zzz.R
@@ -1,4 +1,7 @@
 require(sp)
 .First.lib <- function(lib, pkg) {
 	library.dynam("spgwr", pkg, lib)
+	packageStartupMessage(paste(
+	"NOTE: default kernel and CV criteria changed", 
+	"see help pages for details", sep="\n"), appendLF = FALSE)
 }
