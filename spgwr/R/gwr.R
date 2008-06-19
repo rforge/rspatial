4bbbd18cd1f10b97593452592d19d3abd3c07664
rsbivand <rsbivand@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2008-06-19 04:09:34 +0000

    spurious gwr.e
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rspatial@708 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- spgwr/R/gwr.R
+++ spgwr/R/gwr.R
@@ -284,12 +284,14 @@ print.gwr <- function(x, ...) {
 		lm.i <- lm.wfit(x, y, w.i)
 		df[i, 1] <- sum(w.i)
 		df[i, 2:(m+1)] <- coefficients(lm.i)
-		ei <- residuals(lm.i)
-		df[i, (m+3)] <- ei[i]
+		if (!GWR_args$fp.given && GWR_args$hatmatrix) {
+		    ei <- residuals(lm.i)
+		    df[i, (m+3)] <- ei[i]
 # use of diag(w.i) dropped to avoid forming n by n matrix
 # bug report: Ilka Afonso Reis, July 2005
-		rss <- sum(ei * w.i * ei)
-		df[i, (m+2)] <- 1 - (rss / sum(yiybar * w.i * yiybar))
+		    rss <- sum(ei * w.i * ei)
+		    df[i, (m+2)] <- 1 - (rss / sum(yiybar * w.i * yiybar))
+                } else is.na(df[i, (m+(2:3))]) <- TRUE
 	        if (GWR_args$se.fit) {
 		    p <- lm.i$rank
 		    p1 <- 1:p
