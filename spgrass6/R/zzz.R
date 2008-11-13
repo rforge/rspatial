32e26a0d2de83080c66820e48f1ac08c3d6963ad
rsbivand <rsbivand@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2008-11-13 11:08:10 +0000

    cygwin refresh - no temp flush
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rspatial@745 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- spgrass6/R/zzz.R
+++ spgrass6/R/zzz.R
@@ -4,10 +4,11 @@ if(!exists("Sys.setenv", envir = baseenv()))
    .GRASS_old.GRASS_MESSAGE_FORMAT <- ""
 
 .First.lib <- function(lib, pkg) {
-  if (Sys.getenv("OSTYPE") == "cygwin") {
-    cygwin_clean_temp(verbose=FALSE)
-    packageStartupMessage("Cygwin temp directory flushed\n", appendLF = FALSE)
-  }
+#  if (Sys.getenv("OSTYPE") == "cygwin") {
+#    cygwin_clean_temp(verbose=FALSE)
+#    packageStartupMessage("Cygwin temp directory flushed\n", appendLF = FALSE)
+#  }
+# commented out for GRASS >= 6.3
   require("sp")
   .GRASS_old.GRASS_PAGER <- Sys.getenv("GRASS_PAGER")
   Sys.setenv("GRASS_PAGER"="cat")
@@ -15,6 +16,7 @@ if(!exists("Sys.setenv", envir = baseenv()))
   Sys.setenv("GRASS_MESSAGE_FORMAT"="text")
 
   gisrc <- Sys.getenv("GISRC")
+  loc <- Sys.getenv("LOCATION_NAME")
   if (nchar(gisrc) == 0) gv <- "(GRASS not running)"
   else {
     gv <- Sys.getenv("GRASS_VERSION")
@@ -23,8 +25,13 @@ if(!exists("Sys.setenv", envir = baseenv()))
         gv <- system(paste("g.version", .addexe(), sep=""), intern=TRUE), 
         gv <- system("g.version", intern=TRUE, ignore.stderr=FALSE))
     }
-    loc <- Sys.getenv("LOCATION_NAME")
-    if(nchar(loc) == 0) loc <- read.dcf(gisrc)[1,"LOCATION_NAME"]
+    if(nchar(loc) == 0) {
+      gisrc <- ifelse(.Platform$OS.type == "windows" &&
+                (Sys.getenv("OSTYPE") == "cygwin"), 
+		system(paste("cygpath -w", gisrc, sep=" "), intern=TRUE), 
+		gisrc)
+      loc <- read.dcf(gisrc)[1,"LOCATION_NAME"]
+    }
   }
 
   Smess <- paste('GRASS GIS interface loaded ',
