1c0b886563e4134cee72768ce1500edd77405a99
edzer <edzer@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2009-12-11 16:21:40 +0000

    added the spDists function that wraps (for 2D data) spDistsN1, and returns the distance matrix between two sets of coordinates.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rspatial@884 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ sp/tests/spDists.R
@@ -0,0 +1,13 @@
+library(sp)
+data(meuse)
+data(meuse.grid)
+coordinates(meuse) = ~x+y
+coordinates(meuse.grid) = ~x+y
+x = coordinates(meuse)
+y = coordinates(meuse.grid)
+out = spDists(meuse,meuse.grid)
+out2 = as.matrix(dist(rbind(coordinates(meuse),coordinates(meuse.grid))))
+out2 = out2[1:155,155+1:nrow(y)]
+# should be equal:
+sum(out2 - out)
+summary(as.vector(out2 - out))
