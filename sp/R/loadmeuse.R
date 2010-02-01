29cf7954a68ed08ba3331358645ec958048b2846
edzer <edzer@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2010-02-01 15:17:49 +0000

    functions that loads meuse and meuse.grid to the global env, and converts them to the appropriate Spatial* structures.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rspatial@911 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ sp/R/loadmeuse.R
@@ -0,0 +1,10 @@
+loadMeuse = function(gridded = TRUE) {
+   data(meuse)
+   coordinates(meuse) <<- ~x+y
+   data(meuse.grid)
+   if (gridded) {
+     gridded(meuse.grid) <<- ~x+y
+  } else 
+	 coordinates(meuse.grid) <<- ~x+y
+  invisible(NULL)
+}
