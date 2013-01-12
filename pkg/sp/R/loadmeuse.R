5bff63f30dc85467608626fa541c5dc0d979e261
edzer <edzer@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2013-01-12 13:14:48 +0000

    changed crs to full CRS, as libproj would fill it in
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rspatial@1334 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/sp/R/loadmeuse.R
+++ pkg/sp/R/loadmeuse.R
@@ -1,5 +1,5 @@
 loadMeuse = function(gridded = TRUE, river = FALSE) {
-   crs = CRS("+init=epsg:28992")
+   crs = CRS("+init=epsg:28992 +proj=sterea +lat_0=52.15616055555555 +lon_0=5.38763888888889 +k=0.9999079 +x_0=155000 +y_0=463000 +ellps=bessel +towgs84=565.417,50.3319,465.552,-0.398957,0.343988,-1.8774,4.0725 +units=m +no_defs")
    meuse = NULL
    meuse.grid = NULL
    meuse.riv = NULL
