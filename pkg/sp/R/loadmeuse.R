364838b7f78c71fe8b00e2c8096c3bd12aa09ed6
edzer <edzer@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2013-03-11 11:21:11 +0000

    loadMeuse() now avoids data() without setting envir to local environment(), and uses assign to assign to .GlobalEnv
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rspatial@1358 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/sp/R/loadmeuse.R
+++ pkg/sp/R/loadmeuse.R
@@ -3,20 +3,25 @@ loadMeuse = function(gridded = TRUE, river = FALSE) {
    meuse = NULL
    meuse.grid = NULL
    meuse.riv = NULL
-   data(meuse)
-   coordinates(meuse) <<- ~x+y
-   data(meuse.grid)
+   data("meuse", envir = environment())
+   coordinates(meuse) <- ~x+y
+   proj4string(meuse) <- crs
+   assign("meuse", meuse, envir = .GlobalEnv)
+
+   data("meuse.grid", envir = environment())
    if (gridded) {
-     gridded(meuse.grid) <<- ~x+y
-  } else 
-	 coordinates(meuse.grid) <<- ~x+y
-  if (river) {
-	rm(meuse.riv)
-    data(meuse.riv)
-    meuse.riv <<- SpatialPolygons(list(Polygons(list(Polygon(meuse.riv)),"meuse.riv")))
-    proj4string(meuse.riv) <<- crs
-  }
-  proj4string(meuse) <<- crs
-  proj4string(meuse.grid) <<- crs
-  invisible(NULL)
+     gridded(meuse.grid) <- ~x+y
+   } else 
+	 coordinates(meuse.grid) <- ~x+y
+   proj4string(meuse.grid) <- crs
+   assign("meuse.grid", meuse.grid, envir = .GlobalEnv)
+
+   if (river) {
+     rm(meuse.riv)
+     data("meuse.riv", envir = environment())
+     meuse.riv <- SpatialPolygons(list(Polygons(list(Polygon(meuse.riv)),"meuse.riv")))
+     proj4string(meuse.riv) <- crs
+     assign("meuse.riv", meuse.riv, envir = .GlobalEnv)
+   }
+   invisible(NULL)
 }
