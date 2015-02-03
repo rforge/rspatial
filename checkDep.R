9ce7af978cc46ccde6e3824c6768268b882b8c47
rsbivand <rsbivand@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2015-02-03 19:28:56 +0000

    tidy
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rspatial@1556 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- checkDep.R
+++ checkDep.R
@@ -1,16 +1,20 @@
 depends <- function(pkg = "sp") {
     index <- readLines(sprintf("http://cran.r-project.org/web/packages/%s/index.html", pkg))
-    if( any( grepl("Reverse.*depends", index) ) ){
-        x <- index[ grep("Reverse.*depends", index) + 1L ]
-        gsub( "<.*", "", strsplit( x, "<a href.*?>" )[[1L]] )[-1L]
-    } else character(0L)
+    res <- character(0L)
+    if( any( grepl("Reverse.*depends", index)) || any(grepl("Reverse.*imports", index)) ){
+        if (any(grepl("Reverse.*depends", index))) x <- index[ grep("Reverse.*depends", index) + 1L ]
+        res <- gsub( "<.*", "", strsplit( x, "<a href.*?>" )[[1L]] )[-1L]
+        if (any(grepl("Reverse.*imports", index))) {x <- index[ grep("Reverse.*imports", index) + 1L ]
+        res <- c(res, gsub( "<.*", "", strsplit( x, "<a href.*?>" )[[1L]] )[-1L])}
+    }
+    res 
 }
 
 seen <- character(0)
 graph <- character(0)
 
 rec.depends <- function( pkg ){
-    dep <- depends(pkg)
+    dep <- unique(depends(pkg))
     if( !length(dep) ) return(NULL)
     graph <<- c( graph, sprintf( "%s->%s", pkg, dep ) )
     for(p in dep[!dep %in% seen]) rec.depends( p )
@@ -26,7 +30,8 @@ update.packages(contriburl=url)
 ret = download.packages(seen, destdir = "/tmp", contriburl = url)
 retval = rep(as.logical(NA), nrow(ret))
 for (i in 1:nrow(ret)) {
-	cmd = paste("R CMD check", ret[i,2])
+	cmd = paste("_R_CHECK_FORCE_SUGGESTS_=FALSE R CMD check", ret[i,2])
+#_R_CHECK_FORCE_SUGGESTS_=FALSE 
 	#stopifnot(system(cmd) == 0)
 	retval[i] = system(cmd) != 0
 	#unlink(ret[i,2])
