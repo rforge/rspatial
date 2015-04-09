247a74720a8ae19c2db5266250a73c3d8d91eadb
rsbivand <rsbivand@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2015-04-09 19:16:42 +0000

    tidy
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rspatial@1604 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/sp/build-prep.sh
+++ pkg/sp/build-prep.sh
@@ -4,3 +4,10 @@ sed -e 's/@VERSION@/'$VERSION'/' src/sp.h.in > src/sp.h
 cp src/sp.h inst/include
 cp src/sp_xports.c inst/include
 grep SP_VERSION inst/include/sp.h
+svn up
+sh svn2cl.sh
+fmt < ChangeLog > tmp
+mv tmp ChangeLog
+cp ChangeLog inst
+svn commit -m tidy
+svn up
