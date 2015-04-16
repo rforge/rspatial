c66a8ce9b33cbd8d167361db0174b9cf1f9d6bc2
edzer <edzer@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2015-04-16 19:16:36 +0000

    add build-release script, so that not every build triggers an svn `tidy' commit
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rspatial@1627 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/sp/build-prep.sh
+++ pkg/sp/build-prep.sh
@@ -4,8 +4,3 @@ sed -e 's/@VERSION@/'$VERSION'/' src/sp.h.in > src/sp.h
 cp src/sp.h inst/include
 cp src/sp_xports.c inst/include
 grep SP_VERSION inst/include/sp.h
-svn up
-svn2cl
-cp ChangeLog inst
-svn commit -m tidy inst/ChangeLog
-svn up
