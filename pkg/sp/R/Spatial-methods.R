1565d5a51706634329b08ab1724c17d9a6f82a6c
edzer <edzer@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2015-07-20 10:30:52 +0000

    add webmap examples: OpenStreetMap or google maps from ggmap or RgoogleMaps as background to plot or spplot
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rspatial@1667 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/sp/R/Spatial-methods.R
+++ pkg/sp/R/Spatial-methods.R
@@ -191,11 +191,23 @@ print.summary.Spatial = function(x, ...) {
 #		return(1/cos((mean(ylim) * pi)/180))
 #}
 
+bb2merc = function(x, cls = "ggmap") { # return bbox in the appropriate "web mercator" CRS
+	WGS84 = CRS("+init=epsg:4326")
+	# merc = CRS("+proj=merc +a=6378137 +b=6378137 +lat_ts=0.0 +lon_0=0.0 +x_0=0.0 +y_0=0 +k=1.0 +units=m +nadgrids=@null +wktext +no_defs")
+	merc = CRS("+init=epsg:3857") # http://wiki.openstreetmap.org/wiki/EPSG:3857
+	if (cls == "ggmap") {
+		b = sapply(attr(x, "bb"), c)
+		pts = cbind(c(b[2],b[4]),c(b[1],b[3]))
+	} else if (cls == "RgoogleMaps")
+		pts = rbind(x$BBOX$ll, x$BBOX$ur)[,2:1]
+	else
+		stop("unknown cls")
+	bbox(spTransform(SpatialPoints(pts, WGS84), merc))
+}
 
-
-plot.Spatial <- function(x, xlim=NULL, ylim=NULL, 
+plot.Spatial <- function(x, xlim = NULL, ylim = NULL, 
 	asp = NA, axes = FALSE, bg = par("bg"), ..., 
-	xaxs, yaxs, lab, setParUsrBB=FALSE) {
+	xaxs, yaxs, lab, setParUsrBB = FALSE, bgMap = NULL) {
 
 	bbox <- bbox(x)
 	if (is.null(xlim)) 
@@ -233,6 +245,17 @@ plot.Spatial <- function(x, xlim=NULL, ylim=NULL,
 	}
 	localTitle <- function(..., col, bg, pch, cex, lty, lwd) title(...)
 	localTitle(...)
+	if (!is.null(bgMap)) {
+		if (is(bgMap, "ggmap"))
+			bb = bb2merc(bgMap, "ggmap")
+		else if (all(c("lat.center","lon.center","zoom","myTile","BBOX") %in% names(bgMap))) {
+			# an object returned by RgoogleMaps::GetMap
+			bb = bb2merc(bgMap, "RgoogleMaps")
+			bgMap = bgMap$myTile
+		} else
+			bb = rbind(xlim, ylim)
+		rasterImage(bgMap, bb[1,1], bb[2,1], bb[1,2], bb[2,2], interpolate = FALSE)
+	}
 }
 setMethod("plot", signature(x = "Spatial", y = "missing"), 
 	function(x,y,...) plot.Spatial(x,...))
---
+++ pkg/sp/demo/webmap.R
@@ -0,0 +1,46 @@
+library(sp)
+library(ggmap)
+demo(meuse, ask=F)
+merc = CRS("+init=epsg:3857")
+WGS84 = CRS("+init=epsg:4326")
+
+meuse = spTransform(meuse, WGS84)
+
+bgMap = get_map(as.vector(bbox(meuse)), source = "google", zoom = 13) # needs zoom level
+plot(spTransform(meuse, merc), bgMap = bgMap, pch = 16, cex = .5) # needs mercator for plotting
+
+# spplot with ggmap bg:
+panel.ggmap <- function(map) {
+	bb = sp:::bb2merc(map, "ggmap")
+	grid::grid.raster(map, mean(bb[1,]), mean(bb[2,]), diff(bb[1,]), diff(bb[2,]), 
+		default.units = "native", interpolate = FALSE)
+}
+spplot(spTransform(meuse, merc), c("zinc",  "lead"), colorkey = TRUE,
+	sp.layout = list(panel.ggmap, bgMap, first = TRUE))
+
+bgMap = get_map(as.vector(bbexpand(bbox(meuse), .04)), source = "osm") # WGS84 for background map
+plot(spTransform(meuse, merc), bgMap = bgMap, pch = 16, cex = .5) # needs mercator for plotting
+
+
+# RgoogleMaps:
+center = apply(coordinates(meuse), 2, mean)[2:1]
+library(RgoogleMaps)
+g = GetMap(center=center, zoom=13)
+par(mar = rep(0,4)) # fill full device
+plot(spTransform(meuse, merc), bgMap = g, pch = 16, cex = .5) # needs mercator for plotting
+
+panel.RgoogleMaps <- function(map) {
+	bb = sp:::bb2merc(map, "RgoogleMaps")
+	grid::grid.raster(map$myTile, mean(bb[1,]), mean(bb[2,]), diff(bb[1,]), diff(bb[2,]), 
+		default.units = "native", interpolate = FALSE)
+}
+spplot(spTransform(meuse, merc), c("zinc",  "lead"),
+	sp.layout = list(panel.RgoogleMaps, g, first = TRUE))
+
+# Norway boundary example:
+library(cshapes)
+cshp = cshp(as.Date("2000-01-1"))
+no = cshp[cshp$ISO1AL2 == "NO",]
+
+bgMap = get_map(as.vector(bbox(no))) # WGS84
+plot(spTransform(no, merc), bgMap = bgMap, border = 'red')
